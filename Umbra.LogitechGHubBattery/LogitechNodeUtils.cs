63dd82fa19ec447b94bd10bb87ab4c3eb4dc00a2
Valgrifer <valgrifer123@gmail.com>
2025-10-24 18:00:09 +0200

    Add Widget list status

--- Umbra.LogitechGHubBattery/LogitechNodeUtils.cs
+++ Umbra.LogitechGHubBattery/LogitechNodeUtils.cs
@@ -1,10 +1,40 @@
 using Dalamud.Interface;
+using System;
+using System.Linq;
+using Umbra.Common;
 using Umbra.LogitechGHubBattery.Services;
+using Una.Drawing;
 
 namespace Umbra.LogitechGHubBattery;
 
-internal sealed class LogitechNodeUtils
+internal static class LogitechNodeUtils
 {
+    internal static UdtDocument DocumentFrom(string resourceName)
+    {
+        foreach (var assembly in Framework.Assemblies) {
+            var resource = assembly
+                .GetManifestResourceNames()
+                .FirstOrDefault(r => r.EndsWith(resourceName, StringComparison.OrdinalIgnoreCase));
+            
+            if (resource == null) continue;
+            
+            return UdtLoader.LoadFromAssembly(assembly, resourceName);
+        }
+        
+        throw new Exception($"No UDT document with the name \"{resourceName}\" exists in any assembly.");
+    }
+    
+    internal static FontAwesomeIcon GetMainIcon(DeviceInfo info)
+    {
+        return info.DeviceType switch
+        {
+            "MOUSE" => FontAwesomeIcon.Mouse,
+            "KEYBOARD" => FontAwesomeIcon.Keyboard,
+            "HEADSET" => FontAwesomeIcon.Headset,
+            _ => FontAwesomeIcon.Plug
+        };
+    }
+    
     internal static FontAwesomeIcon GetSecondIcon(BatteryState state)
     {
         if (state.FullyCharged)
